936b69fe4ae295cebe7a870f1afda25949db2124
Lukáš Petrovický <lukas@petrovicky.net>
2023-06-26 12:43:38 +0200

    ci: improve release CI

--- .github/scripts/change_versions.sh
+++ .github/scripts/change_versions.sh
@@ -3,8 +3,8 @@
 # Expects the following environment variables to be set:
 #   $NEW_VERSION             (Example: "1.2.0")
 
-mvn versions:set-property -Dproperty=version.ai.timefold.solver -DnewVersion=$NEW_VERSION
-find . -name build.gradle -exec sed -i -E 's/def optaplannerVersion = \"[^\"\\s]+\"/def optaplannerVersion = \"$NEW_VERSION\"/' {} \;
+find . -name pom.xml -exec sed -i -E 's/<version.ai.timefold.solver>[^<]+<\/version.ai.timefold.solver>/<version.ai.timefold.solver>'"$NEW_VERSION"'<\/version.ai.timefold.solver>/' {} \;
+find . -name build.gradle -exec sed -i -E 's/def timefoldVersion = \"[^\"\\s]+\"/def timefoldVersion = \"'"$NEW_VERSION"'\"/' {} \;
 find . -name pom.xml | xargs git add
 find . -name build.gradle | xargs git add
 git commit -m "chore: switch to version $NEW_VERSION"
