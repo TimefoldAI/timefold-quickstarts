a56cd88340885465f226a1aca5a66a6c6dcb5c82
Frederico Gonçalves <zepfred@users.noreply.github.com>
2024-03-07 12:46:58 -0300

    chore: remove quickstart showcase (#332)
    This pull request removes the `quickstarts-showcase` module and arranges
    the use cases according to the TOC list.

--- runQuickstartsFromSource.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Change directory to the directory of the script
-cd "$(dirname $0)" || exit
-
-cd build
-./mvnw -f quickstarts-showcase quarkus:dev -Dstartup-open-browser=true
-cd ..
