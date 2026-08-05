1248fae80b25dd5d29aa3bbb1e22fbe1741b62e4
Cristiano Nicolai <570894+cristianonicolai@users.noreply.github.com>
2026-08-05 17:00:46 +1000

    feat: add support for moveCountLimit

--- use-cases/conference-scheduling/src/main/java/org/acme/conferencescheduling/support/ObjectHelper.java
+++ use-cases/conference-scheduling/src/main/java/org/acme/conferencescheduling/support/ObjectHelper.java
@@ -3,9 +3,10 @@ package org.acme.conferencescheduling.support;
 import java.util.List;
 
 public final class ObjectHelper {
-    private ObjectHelper() {} // static helper
+    private ObjectHelper() {
+    } // static helper
 
-    public static <T> List<T>  immutableCopy(List<T> list) {
+    public static <T> List<T> immutableCopy(List<T> list) {
         return list == null ? List.of() : List.copyOf(list);
     }
 }
