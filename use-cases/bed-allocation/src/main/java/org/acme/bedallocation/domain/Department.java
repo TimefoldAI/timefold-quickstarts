88c101a1ad2f04cfc36acb0703a49700c33532f8
Tom Cools <tom@timefold.ai>
2026-08-25 15:07:13 +0200

    chore: remove custom equals and hashCode on records

--- use-cases/bed-allocation/src/main/java/org/acme/bedallocation/domain/Department.java
+++ use-cases/bed-allocation/src/main/java/org/acme/bedallocation/domain/Department.java
@@ -2,7 +2,6 @@ package org.acme.bedallocation.domain;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 import ai.timefold.solver.core.api.domain.common.PlanningId;
 
@@ -13,22 +12,6 @@ public record Department(
         Integer maximumAge,
         Map<String, Integer> specialtyToPriority,
         List<Room> rooms) {
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Department department)) {
-            return false;
-        }
-        return Objects.equals(id, department.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
-    }
 
     @Override
     public String toString() {
