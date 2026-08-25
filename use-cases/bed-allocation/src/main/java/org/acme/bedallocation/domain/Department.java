919df4f33121b24fcf93fc4559e160ed5e2f509f
Tom Cools <tom@timefold.ai>
2026-08-25 15:46:21 +0200

    Revert "chore: remove custom equals and hashCode on records"
    This reverts commit 88c101a1ad2f04cfc36acb0703a49700c33532f8.

--- use-cases/bed-allocation/src/main/java/org/acme/bedallocation/domain/Department.java
+++ use-cases/bed-allocation/src/main/java/org/acme/bedallocation/domain/Department.java
@@ -2,6 +2,7 @@ package org.acme.bedallocation.domain;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import ai.timefold.solver.core.api.domain.common.PlanningId;
 
@@ -12,6 +13,22 @@ public record Department(
         Integer maximumAge,
         Map<String, Integer> specialtyToPriority,
         List<Room> rooms) {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Department department)) {
+            return false;
+        }
+        return Objects.equals(id, department.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 
     @Override
     public String toString() {
--- use-cases/bed-allocation/src/main/java/org/acme/bedallocation/domain/Room.java
+++ use-cases/bed-allocation/src/main/java/org/acme/bedallocation/domain/Room.java
@@ -1,6 +1,7 @@
 package org.acme.bedallocation.domain;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 import ai.timefold.solver.core.api.domain.common.PlanningId;
@@ -13,6 +14,22 @@ public record Room(
         GenderLimitation genderLimitation,
         Set<String> equipments,
         List<Bed> beds) {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Room room)) {
+            return false;
+        }
+        return Objects.equals(id, room.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 
     @Override
     public String toString() {
